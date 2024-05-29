3413947fbb6f54728ec4e8ba2fcf5e9e7a3f2cd5
Rov <rodrigounigranrov@gmail.com>
2024-05-29 17:36:04 -0300

    Estudo - microsserviços com springboot - rotas para calculos matemáticos

--- foo/src/main/java/br/com/erudio/MathController.java
+++ foo/src/main/java/br/com/erudio/MathController.java
@@ -1,85 +1,94 @@
 package br.com.erudio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.erudio.com.util.MathUtil;
 import br.com.erudio.exception.UnsuportedMathOperationException;
 
 @RestController
 public class MathController {
+	
+	//Fiz a injeção de dependencia aqui com @Autowired
+	//Com isso, posso utilizar mathUtil sem precisar ficar instanciando na classe.
+	//Na classe MathUtil, utilizei o @Component para que funcione a injeção de dependencia.
+	//Importante, para que funcione corretamente, os pacotes precisam estar dentro da mesa raiz. /src
+	@Autowired
+	MathUtil mathUtil;
 
 	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
+			throws Exception {
+		if (!mathUtil.isNumeric(numberOne) || !mathUtil.isNumeric(numberTwo)) {
 			throw new UnsuportedMathOperationException("Favor, informar apenas valores numericos");
 		}
-		Double sum = convertToDouble(numberOne) + convertToDouble(numberTwo);
+		Double sum = mathUtil.convertToDouble(numberOne) + mathUtil.convertToDouble(numberTwo);
 		return sum;
 	}
-	
+
 	@RequestMapping(value = "/sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-	public Double sub(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+	public Double sub(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
+			throws Exception {
+		if (!mathUtil.isNumeric(numberOne) || !mathUtil.isNumeric(numberTwo)) {
 			throw new UnsuportedMathOperationException("Favor, informar apenas valores numericos");
 		}
-		Double sub = convertToDouble(numberOne) - convertToDouble(numberTwo);
+		Double sub = mathUtil.convertToDouble(numberOne) - mathUtil.convertToDouble(numberTwo);
 		return sub;
 	}
-	
+
 	@RequestMapping(value = "/mult/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-	public Double mult(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+	public Double mult(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
+			throws Exception {
+		if (!mathUtil.isNumeric(numberOne) || !mathUtil.isNumeric(numberTwo)) {
 			throw new UnsuportedMathOperationException("Favor, informar apenas valores numericos");
 		}
-		Double mult = convertToDouble(numberOne) * convertToDouble(numberTwo);
+		Double mult = mathUtil.convertToDouble(numberOne) * mathUtil.convertToDouble(numberTwo);
 		return mult;
 	}
-	
+
 	@RequestMapping(value = "/div/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-	public Double div(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+	public Double div(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
+			throws Exception {
+		if (!mathUtil.isNumeric(numberOne) || !mathUtil.isNumeric(numberTwo)) {
 			throw new UnsuportedMathOperationException("Favor, informar apenas valores numericos");
 		}
-		Double div = convertToDouble(numberOne) * convertToDouble(numberTwo);
+		Double div = mathUtil.convertToDouble(numberOne) / mathUtil.convertToDouble(numberTwo);
 		return div;
 	}
-	
+
 	@RequestMapping(value = "/med/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-	public Double med(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+	public Double med(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
+			throws Exception {
+		if (!mathUtil.isNumeric(numberOne) || !mathUtil.isNumeric(numberTwo)) {
 			throw new UnsuportedMathOperationException("Favor, informar apenas valores numericos");
 		}
 		Double result = null;
 		int rule = 2;
-		Double med = convertToDouble(numberOne) + convertToDouble(numberTwo);
-		result = med/rule;
+		Double med = mathUtil.convertToDouble(numberOne) + mathUtil.convertToDouble(numberTwo);
+		result = med / rule;
 		return result;
 	}
-	
-	@RequestMapping(value = "/source/{number}", method = RequestMethod.GET)
-	public Double source(@PathVariable("number") String number) throws Exception {
-		if (!isNumeric(number)) {
+
+	@RequestMapping(value = "/squareRoot/{number}", method = RequestMethod.GET)
+	public Double squareRoot(@PathVariable("number") String number) throws Exception {
+		if (!mathUtil.isNumeric(number)) {
 			throw new UnsuportedMathOperationException("Favor, informar apenas valores numericos");
 		}
-		Double source = Math.sqrt(convertToDouble(number));
+		Double source = Math.sqrt(mathUtil.convertToDouble(number));
 		return source;
 	}
-	
 
-	private Double convertToDouble(String strNumber) {
-		if (strNumber == null)
-			return 0D;
-		String number = strNumber.replaceAll(",", ".");
-		if (isNumeric(number))
-			return Double.parseDouble(number);
-		return 0D;
+	public Double testaConversao() {
+		String valor = "64.00";
+
+		if (!mathUtil.isNumeric(valor)) {
+			Double valorConvertido = mathUtil.convertToDouble(valor);
+				return valorConvertido;
+		}
+		throw new UnsuportedMathOperationException(valor + "Falhou");
 	}
 
-	private boolean isNumeric(String strNumber) {
-		if (strNumber == null) return false;
-		String number = strNumber.replaceAll(",", ".");
-		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
-	}
 }
